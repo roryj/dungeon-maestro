830e5dcfd452234bec69a10d037aa0ba39b69a36
Rory Jacob <roryj@amazon.com>
2018-11-15 00:25:29 -0800

    Add region and profile name to deploy script

--- sam-app/deploy.sh
+++ sam-app/deploy.sh
@@ -4,9 +4,13 @@ zip main.zip main
 
 sam package --template-file template.yaml \
     --output-template-file packaged.yaml \
-    --s3-bucket dungeon-maestro-repo
+    --s3-bucket dungeon-maestro-repo \
+    --profile roryj \
+    --region us-west-2
 
 sam deploy \
     --template-file packaged.yaml \
     --stack-name dungeon-maestro-stack \
-    --capabilities CAPABILITY_NAMED_IAM
+    --capabilities CAPABILITY_NAMED_IAM \
+    --profile roryj \
+    --region us-west-2
