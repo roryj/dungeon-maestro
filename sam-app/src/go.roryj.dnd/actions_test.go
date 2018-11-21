3597b471d9be245e61457554d3e59850eca449db
Rory Jacob <roryj@amazon.com>
2018-11-21 12:43:36 -0800

    Add base test class

--- sam-app/src/go.roryj.dnd/actions_test.go
+++ sam-app/src/go.roryj.dnd/actions_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewIdentifySpell(t *testing.T) {
+	spell := IdentifySpell{
+		spellName: "Abi Dalzims Horrid Wilting",
+		statBlocks: []string {},
+	}
+
+	result, err := spell.ProcessAction()
+
+	assert.Nil(t, err, "the result should not throw an error")
+	t.Logf("Result: %s. Error: %v", result, err)
+}
