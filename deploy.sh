c54b943ec103bd0d0b6fe267887297e125d26265
Rory Jacob <roryj@amazon.com>
2018-12-09 20:40:49 -0800

    Revert "Remove deploy script"
    This reverts commit 9854f79f03872425efc926d456d26d6fba401b22.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+make clean || exit 1
+make build || exit 1
+
+zip maestro.zip maestro
+
+sam package --template-file template.yaml \
+    --output-template-file packaged.yaml \
+    --s3-bucket dungeon-maestro-repo \
+    --profile roryj \
+    --region us-west-2
+
+sam deploy \
+    --template-file packaged.yaml \
+    --stack-name dungeon-maestro-stack \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --profile roryj \
+    --region us-west-2
